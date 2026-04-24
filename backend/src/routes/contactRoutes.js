bc91b9ce37aa40a694bc8f23ce41e61ce18253be
Habtamu Shewamene <habtamushewamene905@gmail.com>
2026-04-24 12:39:31 -0700

    Remove root backend frontend and src folders

--- backend/src/routes/contactRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { submitContact } = require('../controllers/contactController');
-const { validateContactInput } = require('../utils/validators');
-
-const router = express.Router();
-
-router.post('/', validateContactInput, submitContact);
-
-module.exports = router;
--- frontend/src/services/contactService.js
+++
@@ -1,6 +0,0 @@
-import contactApi from '../api/contactApi';
-
-export const submitContactForm = async (payload) => {
-  const { data } = await contactApi.post('/api/contact', payload);
-  return data;
-};
